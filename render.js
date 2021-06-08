afecb32c176a6afcf9871518ca7b97da788d9f2c
Adhiti <shaili.adhiti@gmail.com>
2021-06-08 16:34:45 +0530

    Modify

--- render.js
+++ render.js
@@ -1,1 +1,16 @@
-// alert("This works!");
+$(document).ready(function () {
+    $(".grid .row .cell").on("click", function () {
+        let clickedCell = this;
+        let rid = $(clickedCell).attr("rid");
+        let cid = $(clickedCell).attr("cid");
+        let col = String.fromCharCode(Number(cid) + 65);
+        let row = Number(rid) + 1
+        let address = col + row;
+        $("#address").val(address);
+        console.log("cell with address", address, "was clicked");
+
+
+        // let cellIdx = $(clickedCell).text();
+        // console.log("cell with index", cellIdx, "was clicked");
+    });
+})
