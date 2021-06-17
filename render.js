d182d4d5d77ff5f33420ff2e0ffc57831e4a9ae3
Adhiti <shaili.adhiti@gmail.com>
2021-06-17 13:24:30 +0530

    New

--- render.js
+++ render.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
 
 
     window.db = [];
+    init();
     $(".grid .row .cell").on("click", function () {
         let clickedCell = this;
         let { rid, cid } = getRCIDFromCell(clickedCell);
@@ -13,12 +14,27 @@ $(document).ready(function () {
         let row = Number(rid) + 1
         let address = col + row;
         $("#address").val(address);
+
         //console.log("cell with address", address, "was clicked");
-
-
+        
+        let cellObject = db[rid][cid];
+        let isEmpty = cellObject.isEmpty;
+        let formula = cellObject.formula;
+        let val = cellObject.val;
+        ("#formula").val(formula);
+        $(`.cell[rid=${rid}][cid=${cid}]`).text(isEmpty ? val : "");
+        
+        //value change
         // let cellIdx = $(clickedCell).text();
-        // console.log("cell with index", cellIdx, "was clicked");
+        console.log("cell with index", address, "was clicked");
     });
+
+    $("#formula").on("blur", function () {
+        let formulaElem = $(this);
+        if (formulaElem.val() == ""){
+            return;
+        }
+    })
 
     $(".grid .row .cell").on("blur", function () {
         let clickedCell = this;
@@ -28,15 +44,29 @@ $(document).ready(function () {
         let row = Number(rid) + 1
         let address = col + row;
         db[rid][cid].val = $(this).text();
+        db[rid][cid].isEmpty = false;
         
     });
     $("#new").on("click", init);
-    $("#save").on("click", async function () {
+    $("#save").on("click", function () {
         // dialog box new file option
-        let sdg = await dialog.showSaveDialog();
-        console.log(sdg.filePath)
+        let sfilepath = dialog.showSaveDialogSync();
+        //console.log(sdg.filePath)
         let data = JSON.stringify(db);
-        fs.promises.writeFile(sdg.filePath,data);
+        fs.writeFileSync(sfilepath,data);
+    })
+    $("#open").on("click", function () {
+        //open dialogBox
+        let fileArr = dialog.showOpenDialogSync();
+        console.log("FileArr", fileArr);
+        //Buffer content
+        let bContent = fs.readFileSync(fileArr[0]);
+        db = JSON.parse(bContent);
+        console.log(db);
+        //Show UI
+        init();
+        setUI(db);
+
     })
 
     function getRCIDFromCell(clickedCell) {
@@ -45,23 +75,39 @@ $(document).ready(function () {
         return { rid: rid, cid: cid }
     }
 
-
-
     function init() {
         let Allrows = $(".grid .row");
         for(let i=0; i < Allrows.length; i++){
             let cols = $(Allrows[i]).find(".cell");
             let colsArr = [];
-            for(let j=0; j < cols.length; j++){
+            for(let j = 0; j < cols.length; j++){
                 let cellObject = {
                     val: 0,
-                    formula: ""
+                    formula: "",
+                    isEmpty: true
                 }
+                $(`.cell[rid=${i}][cid=${j}]`).text();
                 colsArr.push(cellObject);
             }
+           // setUI(db);
             db.push(colsArr);
         }
+        
         console.log(db);
     }
-    init();
+    
+
+    function setUI(db) {
+        let Allrows = $(".grid .row");
+        for(let i=0;i<Allrows.length;i++)
+        {
+            let cols = $(Allrows[i]).find(".cell");
+            for(let j = 0; j < cols.length; j++){
+                let val = db[i][j].val;
+                let isEmpty = db[i][j].isEmpty;
+                $(`.cell[rid=${i}][cid=${j}]`).text(isEmpty ? val : "");
+            }
+            
+        }
+    }
 })
